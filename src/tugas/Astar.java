410ab1632428d6edba7e2d4158fa7a8adc212db0
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-11-03 02:04:18 +0700

    wip

---
+++ src/tugas/Astar.java
@@ -0,0 +1,34 @@
+package tugas;
+
+import tugas.astar.AStarSearch;
+import tugas.astar.Node;
+
+class Astar {
+    public static void main(String[] args) {
+        // Membuat objek Node yang mewakili kota-kota
+        Node jakarta = new Node("Jakarta", 6, 7);
+        Node bandung = new Node("Bandung", 20, 5);
+        Node surabaya = new Node("Surabaya", 9, 5);
+        Node medan = new Node("Medan", 12, 10);
+        Node yogyakarta = new Node("Yogyakarta", 5, 4);
+        Node semarang = new Node("Semarang", 6, 3);
+        Node palembang = new Node("Palembang", 5, 8);
+
+        // Menambahkan tetangga-tetangga kota
+        jakarta.addNeighbor(bandung);
+        jakarta.addNeighbor(semarang);
+        bandung.addNeighbor(jakarta);
+        bandung.addNeighbor(semarang);
+        bandung.addNeighbor(yogyakarta);
+        surabaya.addNeighbor(yogyakarta);
+        surabaya.addNeighbor(semarang);
+        medan.addNeighbor(palembang);
+
+        // Membuat objek AStarSearch
+        AStarSearch astar = new AStarSearch();
+
+        // Menjalankan algoritma A* untuk mencari rute dari bandung ke palembang
+        astar.search(bandung, palembang);
+        astar.printPath(medan);
+    }
+}
